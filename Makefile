b2ebadef5dcbc97da99e562585815920837c6d63
Jimmy Sawczuk <me@jimmysawczuk.com>
2014-02-08 22:26:13 -0500

    Removing go-bindata dependency

--- Makefile
+++ Makefile
@@ -13,8 +13,6 @@ endef
 define build
 	@echo 'Building...'
 
-	git tag --contains HEAD | go-bindata -f="getVersion" -p="static" > src/static/version.go
-
 	go install scm
 	go install static
 
--- src/static/static.go
+++ src/static/static.go
@@ -1,22 +1,7 @@
 package static
 
-import (
-	"bytes"
-	"strings"
-)
+var version = "v1.0.2"
 
 func Version() string {
-
-	tags_list := bytes.NewBuffer(getVersion()).String()
-	tags_list = strings.Replace(tags_list, `\r\n`, `\n`, 0)
-	tags := strings.Split(tags_list, `\n`)
-
-	for _, tag := range tags {
-		tag = strings.TrimSpace(tag)
-		if strings.HasPrefix(tag, "v") {
-			return tag
-		}
-	}
-
-	return ""
+	return version
 }
