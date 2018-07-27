37554970c1d39b67a8f3bdbf6d9aa031c1e69330
Jimmy Sawczuk <github@jimmysawczuk.com>
2018-07-26 20:41:00 -0400

    Removing docker build script

--- docker-build.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p build
-GOOS=linux GOARCH=amd64 go build -o build/scm-status-linux-amd64
-docker build -t jimmysawczuk/scm-status .
-docker push jimmysawczuk/scm-status
-rm -rf build
